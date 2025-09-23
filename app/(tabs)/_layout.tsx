872f7646ed8341a8bcecd480cbfce5b16609718e
rlawlsdn263 <rlawlsdn263@naver.com>
2025-09-23 14:15:40 +0900

    feat: tabBarIcon 속석으로 하단 네비게이션 커스터마이징

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,19 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text } from "react-native";
+
+const TabIcons = () => {
+  return (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full items-center justify-center min-w-[112px] min-h-14 mt-4 rounded-full overflow-hidden"
+    >
+      <Image source={icons.home} tintColor="#151312" className="size-5" />
+      <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+    </ImageBackground>
+  );
+};
 
 export default function TabsLayout() {
   return (
@@ -8,6 +23,7 @@ export default function TabsLayout() {
         options={{
           title: "홈",
           headerShown: true,
+          tabBarIcon: ({ focused }) => <TabIcons />,
         }}
       />
       <Tabs.Screen
@@ -15,6 +31,7 @@ export default function TabsLayout() {
         options={{
           title: "저장",
           headerShown: true,
+          tabBarIcon: ({ focused }) => <TabIcons />,
         }}
       />
       <Tabs.Screen
@@ -22,6 +39,7 @@ export default function TabsLayout() {
         options={{
           title: "검색",
           headerShown: true,
+          tabBarIcon: ({ focused }) => <TabIcons />,
         }}
       />
       <Tabs.Screen
@@ -29,6 +47,7 @@ export default function TabsLayout() {
         options={{
           title: "프로필",
           headerShown: true,
+          tabBarIcon: ({ focused }) => <TabIcons />,
         }}
       />
     </Tabs>
