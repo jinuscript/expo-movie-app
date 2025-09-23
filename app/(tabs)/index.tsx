36f0b615c71550841544a9cd57b1b0c982d16214
jinuscript <jinuscript@gmail.com>
2025-09-23 15:43:34 +0900

    feat: ScrollView의 스타일을 className과 contentContainerStyle로 변경

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>홈</Text>
+    <View className="flex-1 bg-primary">
+      {/* 배경 */}
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        {/* 아이콘 */}
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 };
