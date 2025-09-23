49cf84f25e9eebb5eae8bd1f400eff64e44cb603
rlawlsdn263 <rlawlsdn263@naver.com>
2025-09-23 14:27:47 +0900

    feat: 하단 네비게이션 스타일링

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,28 @@ const TabIcons = ({ focused, icon, title }) => {
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
