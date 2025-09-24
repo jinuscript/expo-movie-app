4808087b062542756cc5d467125008dbd57f4817
rlawlsdn263 <rlawlsdn263@naver.com>
2025-09-24 16:35:52 +0900

    feat: 영화 데이털르 호출하는 API 함수 추가

--- services/api.ts
+++ services/api.ts
@@ -6,3 +6,21 @@ export const TMDB_CONFIG = {
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = `/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
