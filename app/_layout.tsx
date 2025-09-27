4ef3155413867a05ec30a021ba9cba513d4731d5
rlawlsdn263 <rlawlsdn263@naver.com>
2025-09-27 22:26:31 +0900

    refactor: movies/[id] 스택 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
       <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
       </Stack>
     </>
   );
